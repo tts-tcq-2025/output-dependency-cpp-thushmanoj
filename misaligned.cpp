598b7c445281a66814f141373b180a8c3e18e2c6
Thushara <62303832+thushmanoj@users.noreply.github.com>
2025-08-08 09:14:37 +0530

    Update_1 misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
-#include <assert.h>
+#include <vector>
+#include <string>
+#include <cassert>
 
-int printColorMap() {
+struct ColorMap {
+    int number;
+    std::string majorColor;
+    std::string minorColor;
+};
+
+std::vector<ColorMap> generateColorMap()
+{
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+
+    std::vector<ColorMap> colorMapGen;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            colorMapGen.push_back({i * 5 + j, majorColor[i], minorColor[j]});
         }
     }
-    return i * j;
+    return colorMapGen;
+}
+
+void printColorMap(const std::vector<ColorMap>& genColorMap) {
+    for (const auto& pair : genColorMap) {
+        std::cout << pair.number << " | "
+                  << pair.majorColor << " | "
+                  << pair.minorColor << std::endl;
+    }
 }
 
 void testPrintColorMap() {
     std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap();
-    assert(result == 25);
+    auto genColorMap = generateColorMap();
+    printColorMap(genColorMap);
+    //int result = printColorMap();
+    assert(genColorMap.size() == 25);
+    //Test for first pair
+    assert(genColorMap[0].number == 0);
+    assert(genColorMap[0].majorColor == "White");
+    assert(genColorMap[0].minorColor == "Blue");
     std::cout << "All is well (maybe!)\n";
+
 }
